801470112f43ea33efc6f2497aac9758ef6b6d98
Arturo Melgarejo Galindo <arturomelgarejogalindo@gmail.com>
2020-05-13 18:26:20 -0700

    modified:   client3.py 	modified:   crypto/AES.json 	new file:   crypto/IV 	modified:   crypto/hosts.csv 	modified:   servidor.py

--- client3.py
+++ client3.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3.7
 import socket
 from passlib.hash import argon2
 from Crypto.Cipher import AES
 import json
-from Crypto import Random
+from hashlib import new
+from os import urandom
 class Alertas:
     def info(self, log):
         return print("\033[94m" + "[+] Info: " + log + "\033[0m")
         pass
     def warning(self, log):
-        return print("\033[93m" + "[+] Warning: " + log + "\033[0m")
+        return print("\033[93m" + "[+] Importante: " + log + "\033[0m")
         pass
     def error(self, log):
         return print("\033[91m" + "[+] Error: " + log + "\033[0m")
@@ -26,42 +28,66 @@ class Codigos_mensajes:
     }
     pass
 
+def tob(a):
+    return bytes(a, 'utf-8')
 
-def mandar_contenido(contenido="Defecto", code=b"\x00"):
+
+def mandar_contenido(contenidoi=b"1234567891234567", code=b"\x00"):
+    """Estaba muy dormido cuando lo hice, lo solucione asi y ya XD, pero bueno, la estructura funciona a la perfeccion y el sistema de firmado"""
+    contenido = contenidoi
     if len(info[0]) <= 0:
         return alert.error("No se ha encontrado ningun host en las claves")
-    a = encriptar(contenido, info[1])
-    mensaje = code + a
-    #STRUCT mensaje =
-    #STRUCT = length(mensaje) + , 0x00, mensaje + checksum
-    s.send(bytes([len(mensaje)]) + mensaje + argon2.hash(contenido))
-    print(bytes([len(mensaje)]) + mensaje + argon2.hash(contenido))
-    return info[0], contenido, info[1]
+    if len(contenido) < 16:
+        padding2 = 16-len(contenido)
+        contenido = contenido + urandom(padding2)
+        a = True
+    padding = len(contenido) % 16
+    if padding == 0:
+        pass
+    else:
+        contenido = contenido + urandom(16-padding)
+        a = False
+    if a:
+        padding = padding2
+    else:
+        padding = 16-padding
+    alert.info("Bloque de cifrado Corregido.")
+    a = encriptar(contenido)
+    #STRUCT mensaje = mensaje + relleno
+    #STRUCT = code + length(mensaje) + mensaje + len(randompadding) + checksum
+    argon = argon2.hash(contenidoi)
+    s.send(code + bytes([len(a)]) + a + bytes([padding]) + bytes(argon, "utf-8"))
 
 
-def encriptar(mensaje, key):
-    b = Random.new().read(AES.block_size)
-    cifrado = AES.new(key, AES.MODE_CFB, b)
-    return cifrado.encrypt(mensaje)  # mensaje cifrado y de la key para en caso de que no exista una clave->
+def encriptar(mensaje):
+    cifrado = AES.new(key, AES.MODE_CBC, Init_Vector)
+    return cifrado.encrypt(mensaje)  # mensaje cifrado
 
 
-def desdencriptar(mensaje, key):
-    clave = AES.new(key)
+def desdencriptar(mensaje):
+    clave = AES.new(key, AES.MODE_CBC, Init_Vector)
+    """Desencriptamos, con la key guardada, y el Init Vector"""
     return clave.decrypt(mensaje)
 
-
-# de ese host, pasarla a la funcion para que pueda trabajar con ella
+def rwiv(md5hash, mode):
+    with open("crypto/IV", mode) as archivo:
+        if mode == "wb":
+            return archivo.write(md5hash)
+        else:
+            return archivo.read(16)
 def leer_json():
     with open('crypto/AES.json') as archivo_json:  # abrimos el json
         datos = json.load(archivo_json)  # abrimos el archivo como json
-        return datos['key'], datos['servidor']
+        return datos['key'], datos['servidor'], datos['nombre'], datos['Init Vector']
 
 
-def escribir_json(key, host):
+def escribir_json(keyiv, host, iv):
     with open('crypto/AES.json', 'w') as archivo_json:
         data = {}
-        data['key'] = key  # Escribimos la clave en el json
+        data['key'] = keyiv  # Escribimos la clave en el json
         data['servidor'] = host # Escribimos el host en el registro
+        data['nombre'] = info[2] or "Client"
+        data['Init Vector'] = iv
         json.dump(data, archivo_json)
 
 
@@ -69,6 +95,8 @@ def conectar_servidor():
     global s
     global alert
     global info
+    global Init_Vector
+    global key
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     info = leer_json()
     host = socket.gethostname()
@@ -77,23 +105,33 @@ def conectar_servidor():
     if len(info[1]) > 1:
         host = info[1]
     alert.info("Intentando conectar al servidor: " + host)
-    s.connect((host, 2872))
+    s.connect((host, 2876))
     mensaje = ''
+    """Init vector, No necesita ser para nada seguro, simplemente afecta a la primera rotacion de bits, lo importante es la key, el IV se hace alrededor de algo que identifique al cliente"""
     while True:
         msg = s.recv(1024)
         if msg.startswith(code.exchange_key):  # si nos llega el header del 1º intercambio de clave
             msg = msg.replace(code.exchange_key, b"")
             if not info[0]:  # escribir_json(mensaje)
-                alert.info("Guardando la clave en el registro AES json...")
-                escribir_json(msg.decode('utf-8'), host)
+                iv = new("md5", info[2].encode("utf-8"))
+                IV = iv.hexdigest()
+                iv = iv.digest()
+                alert.info("IV vector: " + IV)
+                alert.warning("Guardando la key AES & IV")
+                k = msg.decode('utf-8')
+                escribir_json(k[0:32], host, IV)
+                rwiv(iv, "wb")
             else:
                 alert.alerta_seguridad("¡ATENCION! SE HA INTENTADO HACER UN INTERCAMBIO DE CLAVES, PERO YA HAY CLAVES EXISTENTES EN EL CLIENTE, \nSE REPORTARA AL ADMINISTRADOR DEL SERVIDOR.\n")
                 """En caso de que se pretenda un intercambio de clave erroneo, se informara,
                  ya que puede ser un atacante mitm pretendiendo un intercambio de claves"""
         else:
-            mandar_contenido(info)
-            print("Mensaje Recibido:", msg)
-            print("Mensaje desencriptado:", desdencriptar(msg, info[0]))
+            key = info[0]
+            Init_Vector = rwiv("", "rb")
+            inputs = input("\033[94m" + "Prueba de concepto! Envia mensajes Firmados por Argon2 al servidor $ " + "\033[0m")
+            mandar_contenido(contenidoi=bytes(inputs, "utf-8"))
+            exit("Adios")
+            #alert.info("Paquete del servidor Recibido: ", desdencriptar(msg))
         break
 
 
--- servidor.py
+++ servidor.py
@@ -1,71 +1,139 @@
 #!/usr/bin/env python3.7
 import socket
+from passlib.hash import argon2
 from Crypto.Cipher import AES
 import random
 import string
-import struct
+from hashlib import new
 import csv
-import threading
 from signal import signal, SIGINT
 from sys import exit
+"""¡Encriptacion Simetrica firmada! Creada Por: Arturo Melgarejo Galindo"""
+
+
+
+class Alertas:
+    def info(self, log):
+        return print("\033[94m" + "[+] Info: " + log + "\033[0m")
+        pass
+    def warning(self, log):
+        return print("\033[93m" + "[+] Importante: " + log + "\033[0m")
+        pass
+    def error(self, log):
+        return print("\033[91m" + "[+] Error: " + log + "\033[0m")
+        pass
+    def alerta_seguridad(self, log):
+        return print("\033[91m" + "[+] Alerta de seguridad/error: " + log + "\033[0m")
+
+
+def rwiv(md5hash, mode):
+    with open("crypto/IV", mode) as archivo:
+        if mode == "wb":
+            return archivo.write(md5hash)
+        else:
+            return archivo.read(16)
+
+
 def handler(signal_received, frame):
     print(' CTRL+C DETECTADO ADIOS MI CAPITAN :)')
     exit(0)
+
+
 if __name__ == '__main__':
     signal(SIGINT, handler)
-def mandar_contenido(hostkeys, contenido="Mensaje Por defecto"):
-    if len(hostkeys[2]) <= 0:
-        print("¡Estamos intentando mandar un mensaje encriptado sin tener al host en las keys!")
+
+
+def mandar_contenido(contenido="Mensaje Por defecto"):
+    if len(col[2]) <= 0:
+        alert.error("¡Estamos intentando mandar un mensaje encriptado sin tener al host en las keys!")
         pass
-    a = encriptar(contenido, hostkeys[1])
-    return a, hostkeys[1], contenido, hostkeys[2]
+    a = encriptar(contenido, col[1])
+
+
 def escribir_csv(lista):
-        with open('crypto/hosts.csv', 'a+', newline='') as archivo:
-            escribir = csv.writer(archivo, delimiter=";")
-            escribir.writerow(lista)
+    with open('crypto/hosts.csv', 'a+', newline='') as archivo:
+        escribir = csv.writer(archivo, delimiter=";")
+        return escribir.writerow(lista)
+
+
 def buscarcsv(host):
-        with open('crypto/hosts.csv') as archivo:
-            csvreader = csv.reader(archivo, delimiter=';')
-            contador = 0
-            for col in csvreader:
-                if len(col[0]) <= 0:
-                    print("Primer Host de intercambio")
-                    return False, 1
-                if col[0] == host:
-                    print("Host encontrado en lista de claves-hosts")
-                    return col[0], col[1], col[2]
-            print("No se ha encontrado el host indicado en nuestras columnas")
-            return False, 2
+    global col
+    with open('crypto/hosts.csv') as archivo:
+        csvreader = csv.reader(archivo, delimiter=';')
+        contador = 0
+        for col in csvreader:
+            if col[0] == host:
+                alert.info("Host encontrado en lista de claves-hosts")
+                return
+        alert.warning("Primer Intercambio de clave simetrica (AES)")
+        col[0] = ""
+        return False
+
+
 def passgenerator(stringLength):
-        #generador de pass aleatoria // password ascii generator
-        letras = string.ascii_letters
-        return ''.join(random.choice(letras) for i in range(stringLength))
-def encriptar(mensaje, key):
-        clave = AES.new(key)
-        return clave.encrypt(mensaje)
-def desdencriptar(mensaje, key):
-        clave = AES.new(key)
-        return clave.decrypt(mensaje)
-def enviar():
-        max_hosts = 5 #archivo de configuracion decide los maximos hosts de la red que van a conectarse // Max host from config file
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((socket.gethostname(), 2872))
-        s.listen(max_hosts)
-        while True:
-                paquete, address = s.accept()
-                Aes_keys = True #borrar despues de crear las columnas de csv
-                print("successful connection From -> {}", address[0])
-                hostkeys = buscarcsv(address[0])
-                if hostkeys[0] == False: #en caso de que el host no este en nuestras columnas de cifrados,
-                        #le mandamos el header de cifrado y el host ya tomara decision
-                        header = "ZEXjNHTJrgo12xcVWrdGvvvHfoHzSv" #in case of host isn't in csv columns of ciphers
-                        key = bytes(passgenerator(32), "utf-8")
-                        lista = [address[0], key.decode("utf-8"), "Client"]
-                        escribir_csv(lista)
-                        print("Key exchange from -> ", address[0], "Key-content -> ", key.decode("utf-8")) #print to logs the key exchange is going to be
-                        paquete.send(bytes(header, "utf-8") + key) #we send header + cipher key
-                        paquete.close()
-                else:
-                        tracker = mandar_contenido(hostkeys, contenido=passgenerator(16))
-                        paquete.send(tracker[0])
-enviar()
+    # generador de pass aleatoria // password ascii generator
+    letras = string.ascii_letters
+    return ''.join(random.choice(letras) for i in range(stringLength))
+
+
+def encriptar(mensaje):
+    cifrado = AES.new(col[1], AES.MODE_CBC, Init_Vector)
+    """Encriptamos el texto con el IV, CBC & k (key)"""
+    return cifrado.encrypt(mensaje)
+
+
+def desdencriptar(mensaje):
+    clave = AES.new(col[1], AES.MODE_CBC, Init_Vector)
+    """Desencriptamos, con la key guardada, y el Init Vector"""
+    return clave.decrypt(mensaje)
+
+def unpack_func(pa):
+    code = pa[0:1]
+    longitud_men = pa[1:2]
+    resp = pa[2:longitud_men[0]+2]
+    longitud_pad = pa[longitud_men[0]+2:longitud_men[0]+3]
+    hash = pa[longitud_men[0]+3:len(pa)]
+    resp = desdencriptar(resp)
+    resp = resp[0:len(resp)-longitud_pad[0]]
+    if argon2.verify(resp, hash):
+        alert.warning('Se ha comprobado la integridad del paquete recibido')
+    else:
+        alert.alerta_seguridad("¡Atencion no se ha verificado la integridad del mensaje, alguien puede estar haciendo un mitm! Se reportara al Administrador del servidor")
+
+
+def main():
+    global alert
+    global Init_Vector
+    max_hosts = 5  # archivo de configuracion decide los maximos hosts de la red que van a conectarse // Max host from config file
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    alert = Alertas()
+    alert.info("Intentando Bindear el puerto 2876")
+    s.bind((socket.gethostname(), 2876))
+    s.listen(max_hosts)
+    while True:
+        paquete, address = s.accept()
+        resultado = buscarcsv(address[0])
+        if len(col[0]) <= 0:  # en caso de que el host no este en nuestras columnas de cifrados,
+            # le mandamos el header de cifrado y el host ya tomara decision
+            alert.info("Intercambio de claves con el host: " + address[0])
+            header = "ZEXjNHTJrgo12xcVWrdGvvvHfoHzSv"  # in case of host isn't in csv columns of ciphers
+            key = bytes(passgenerator(32), "utf-8")
+            lista = [address[0], key.decode("utf-8"), "Client"]
+            escribir_csv(lista)
+            iv = new("md5", col[2].encode("utf-8"))
+            IV = iv.hexdigest()
+            iv = iv.digest()
+            alert.info("IV vector: " + IV)
+            rwiv(iv, "wb")
+            # print to logs the key exchange is going to be
+            paquete.send(bytes(header, "utf-8") + key)  # we send header + cipher key
+            paquete.close()
+        else:
+            paquete.send(b"Hola buenos dias")
+            Init_Vector = rwiv("", "rb")
+            contenido = paquete.recv(1024)
+            unpack_func(contenido)
+
+
+main()
